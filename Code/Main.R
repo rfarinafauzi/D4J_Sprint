3ee3190fdc016ac2d0cb42158d534fbac3711c47
rfarinafauzi <rfbmfr1@st-andrews.ac.uk>
2025-12-04 09:12:11 +0800

    Plot data

--- Code/Main.R
+++ Code/Main.R
@@ -7,3 +7,10 @@ library(dplyr)
 library(readxl)
 
 my_data <- read_excel("Data/Olympics 100m Winning Times.xlsx")
+
+ggplot(my_data, aes(x=`Olympic year`)) + 
+  geom_point(aes(y=`Time_M`, colour = "darkred")) + 
+  geom_point(aes(y=`Time_W`, colour = "blue")) + 
+  labs(x="Olympic Year", y="Winning Times", title="Olympics Winning Time (100m Event)") + 
+  scale_color_identity(name='', breaks=c('darkred', 'blue'), labels=c("Men", "Women"), guide='legend')
+
